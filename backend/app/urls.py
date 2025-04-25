c53bdd82e3fe62480d110da9798a6f699c3da67d
Rodrigo-Flores <dev.rodrigoflores@gmail.com>
2025-04-24 21:23:43 -0400

    feat(urls): :sparkles: Refactor URL patterns to include user profile and role update paths

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,23 +1,8 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from users.views import SignUpView, dashboard_view, manage_users
+from django.views.generic.base import RedirectView
+from users.views import SignUpView, dashboard_view, manage_users, update_user_roles, profile_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +11,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('dashboard/', dashboard_view, name='dashboard'),
     path('users/', manage_users, name='manage_users'),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
+    path('usuarios/', manage_users, name='manage_users'),
+    path('usuarios/update_roles/', update_user_roles, name='update_user_roles'),
+    path('profile/', profile_view, name='profile'),
 ]
